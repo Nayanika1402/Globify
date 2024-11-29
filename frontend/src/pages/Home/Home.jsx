4a10545969e0bc8022e94d4bb37ead1201b2b693
Nayanika1402 <132455412+Nayanika1402@users.noreply.github.com>
2024-11-29 17:19:59 +0530

    Home page updated(2)

--- frontend/src/pages/Home/Home.jsx
+++ frontend/src/pages/Home/Home.jsx
@@ -1,26 +1,25 @@
-import React, {useState,useEffect} from 'react';
-import NavBar from"../../components/NavBar";
-import {useNavigate} from "react-router-dom";
+import React, { useState, useEffect } from 'react';
+import NavBar from "../../components/NavBar";
+import TravelStoryCard from '../../components/Cards/TravelStoryCard';
+import { useNavigate } from "react-router-dom";
 import axiosInstance from "../../utils/axiosInstance";
 
 const Home = () => {
   const navigate = useNavigate();
-
-  const[userInfo, setUserInfo] = useState(null);
+  const [userInfo, setUserInfo] = useState(null);
   const [allStories, setAllStories] = useState([]);
 
   // Get User Info
-  const getUserInfo = async() => {
+  const getUserInfo = async () => {
     try {
-
       const response = await axiosInstance.get("/get-user");
-      if(response.data && response.data.user) {
+      if (response.data && response.data.user) {
         // Set user if data exists
         setUserInfo(response.data.user);
       }
-    }catch (error) {
-      if(error.response.status === 401) {
-        //Clear storage if unauthorized
+    } catch (error) {
+      if (error.response.status === 401) {
+        // Clear storage if unauthorized
         localStorage.clear();
         navigate("/login"); // Redirect to login
       }
@@ -31,14 +30,22 @@ const Home = () => {
   const getAllTravelStories = async () => {
     try {
       const response = await axiosInstance.get("/get-all-stories");
-      if(response.data && response.data.stories) {
+      if (response.data && response.data.stories) {
         setAllStories(response.data.stories);
       }
-    }catch (error) {
-      console.log("An unexpected error occured.Please try again.");
-
+    } catch (error) {
+      console.log("An unexpected error occurred. Please try again.");
     }
-  }
+  };
+
+  // Handle Edit Story click
+  const handleEdit = (data) => {}
+
+  // Handle Travel Story Click
+  const handleViewStory = (data) => {}
+
+  // Handle Update Favourite
+  const updateIsFavourite = async(storyData) => {}
 
   useEffect(() => {
     getAllTravelStories();
@@ -49,16 +56,38 @@ const Home = () => {
 
   return (
     <>
-    <NavBar userInfo = {userInfo} />
+      <NavBar userInfo={userInfo} />
 
-    <div className="container mx-auto py-10">
-      <div className ="flex gap-7">
-        <div className ="flex-1">
+      <div className="container mx-auto py-10">
+        <div className="flex gap-7">
+          <div className="flex-1">
+            {allStories.length > 0 ? (
+              <div className="grid grid-cols-2 gap-4">
+                {allStories.map((item) => {
+                  return (
+                    <TravelStoryCard
+                      key={item._id}
+                      imgUrl={item.imageUrl}
+                      title={item.title}
+                      story={item.story}
+                      date={item.visitedDate}
+                      visitedLocation={item.visitedLocation}
+                      isFavourite={item.isFavourite}
+                      onEdit={() => handleEdit(item)}
+                      onClick={() => handleViewStory(item)}
+                      onFavouriteClick={() => updateIsFavourite(item)}
+                    />
+                  );
+                })}
+              </div>
+            ) : (
+              <>Empty</>
+            )}
+          </div>
 
           <div className="w-[320px]"></div>
         </div>
       </div>
-    </div>
     </>
   );
 };
